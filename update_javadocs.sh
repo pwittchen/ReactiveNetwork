154ac74f75431c0295725fceb46a8929f45a0db1
Piotr Wittchen <piotr@wittchen.biz.pl>
2018-02-11 09:29:35 +0100

    adding update_javadocs.sh script

---
+++ update_javadocs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# update javadocs for RxJava2.x
+git checkout RxJava2.x
+./gradlew clean androidJavadocs
+git checkout gh-pages
+rm -rf javadoc/RxJava2.x/*
+cp -avr library/build/docs/javadoc/* ./javadoc/RxJava2.x
+echo "javadocs for RxJava2.x updated"
+
+# update javadocs for RxJava1.x
+git checkout RxJava1.x
+./gradlew clean androidJavadocs
+git checkout gh-pages
+rm -rf javadoc/RxJava1.x/*
+cp -avr library/build/docs/javadoc/* ./javadoc/RxJava1.x
+echo "javadocs for RxJava1.x updated"
+
+echo "javadocs for both RxJava1.x and RxJava2.x updated - now you can commit and push your changes"
