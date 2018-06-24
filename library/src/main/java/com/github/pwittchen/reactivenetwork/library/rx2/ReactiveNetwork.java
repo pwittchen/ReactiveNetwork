f906b8b7a7d6115fb84599ca1f75a3320b26639b
Piotr Wittchen <piotr@wittchen.io>
2018-06-24 18:31:20 +0200

    The following changes were applied (solves issue #271): - introducing InternetObservingSettings with Builder pattern - removing confusing methods with various multiple parameters in the ReactiveNetwork class (API-breaking changes) - introducing new methods with Settings parameter, which can be constructed via new builder - updating unit tests

--- library/src/main/java/com/github/pwittchen/reactivenetwork/library/rx2/ReactiveNetwork.java
+++ library/src/main/java/com/github/pwittchen/reactivenetwork/library/rx2/ReactiveNetwork.java
@@ -18,10 +18,9 @@ package com.github.pwittchen.reactivenetwork.library.rx2;
 import android.Manifest;
 import android.content.Context;
 import android.support.annotation.RequiresPermission;
+import com.github.pwittchen.reactivenetwork.library.rx2.internet.observing.InternetObservingSettings;
 import com.github.pwittchen.reactivenetwork.library.rx2.internet.observing.InternetObservingStrategy;
-import com.github.pwittchen.reactivenetwork.library.rx2.internet.observing.error.DefaultErrorHandler;
 import com.github.pwittchen.reactivenetwork.library.rx2.internet.observing.error.ErrorHandler;
-import com.github.pwittchen.reactivenetwork.library.rx2.internet.observing.strategy.WalledGardenInternetObservingStrategy;
 import com.github.pwittchen.reactivenetwork.library.rx2.network.observing.NetworkObservingStrategy;
 import com.github.pwittchen.reactivenetwork.library.rx2.network.observing.strategy.LollipopNetworkObservingStrategy;
 import com.github.pwittchen.reactivenetwork.library.rx2.network.observing.strategy.MarshmallowNetworkObservingStrategy;
@@ -36,11 +35,6 @@ import io.reactivex.Single;
  */
 public class ReactiveNetwork {
   public final static String LOG_TAG = "ReactiveNetwork";
-  private static final String DEFAULT_PING_HOST = "http://clients3.google.com/generate_204";
-  private static final int DEFAULT_PING_PORT = 80;
-  private static final int DEFAULT_PING_INTERVAL_IN_MS = 2000;
-  private static final int DEFAULT_INITIAL_PING_INTERVAL_IN_MS = 0;
-  private static final int DEFAULT_PING_TIMEOUT_IN_MS = 2000;
 
   protected ReactiveNetwork() {
   }
@@ -109,108 +103,25 @@ public class ReactiveNetwork {
    */
   @RequiresPermission(Manifest.permission.INTERNET)
   public static Observable<Boolean> observeInternetConnectivity() {
-    return observeInternetConnectivity(DEFAULT_INITIAL_PING_INTERVAL_IN_MS,
-        DEFAULT_PING_INTERVAL_IN_MS, DEFAULT_PING_HOST, DEFAULT_PING_PORT,
-        DEFAULT_PING_TIMEOUT_IN_MS, new DefaultErrorHandler());
+    InternetObservingSettings settings = InternetObservingSettings.create();
+    return observeInternetConnectivity(settings.strategy(), settings.initialInterval(),
+        settings.interval(), settings.host(), settings.port(),
+        settings.timeout(), settings.errorHandler());
   }
 
   /**
-   * Observes connectivity with the Internet with default settings,
-   * but with custom InternetObservingStrategy. It pings remote host
-   * (www.google.com) at port 80 every 2 seconds with 2 seconds of timeout. This operation is used
-   * for determining if device is connected to the Internet or not. Please note that this method is
-   * less efficient than {@link #observeNetworkConnectivity(Context)} method and consumes data
-   * transfer, but it gives you actual information if device is connected to the Internet or not.
+   * Observes connectivity with the Internet in a given time interval.
    *
-   * @param strategy which implements InternetObservingStrategy
-   * @return RxJava Observable with Boolean - true, when we have an access to the Internet
-   * and false if not
+   * @param settings Internet Observing Settings created via Builder pattern
+   * @return RxJava Observable with Boolean - true, when we have connection with host and false if
+   * not
    */
   @RequiresPermission(Manifest.permission.INTERNET)
   public static Observable<Boolean> observeInternetConnectivity(
-      final InternetObservingStrategy strategy) {
-    checkStrategyIsNotNull(strategy);
-    return strategy.observeInternetConnectivity(DEFAULT_INITIAL_PING_INTERVAL_IN_MS,
-        DEFAULT_PING_INTERVAL_IN_MS, strategy.getDefaultPingHost(), DEFAULT_PING_PORT,
-        DEFAULT_PING_TIMEOUT_IN_MS, new DefaultErrorHandler());
-  }
-
-  /**
-   * Observes connectivity with the Internet with default settings,
-   * but with custom InternetObservingStrategy and host. It pings remote host
-   * at port 80 every 2 seconds with 2 seconds of timeout. This operation is used
-   * for determining if device is connected to the Internet or not. Please note that this method is
-   * less efficient than {@link #observeNetworkConnectivity(Context)} method and consumes data
-   * transfer, but it gives you actual information if device is connected to the Internet or not.
-   *
-   * @param strategy which implements InternetObservingStrategy
-   * @param host for checking Internet connectivity
-   * @return RxJava Observable with Boolean - true, when we have connection with host and false if
-   * not
-   */
-  public static Observable<Boolean> observeInternetConnectivity(
-      final InternetObservingStrategy strategy, final String host) {
-    checkStrategyIsNotNull(strategy);
-    return strategy.observeInternetConnectivity(DEFAULT_INITIAL_PING_INTERVAL_IN_MS,
-        DEFAULT_PING_INTERVAL_IN_MS, host, DEFAULT_PING_PORT, DEFAULT_PING_TIMEOUT_IN_MS,
-        new DefaultErrorHandler());
-  }
-
-  /**
-   * Observes connectivity with the Internet in a given time interval.
-   *
-   * @param intervalInMs in milliseconds determining how often we want to check connectivity
-   * @param host for checking Internet connectivity
-   * @param port for checking Internet connectivity
-   * @param timeoutInMs for pinging remote host in milliseconds
-   * @return RxJava Observable with Boolean - true, when we have connection with host and false if
-   * not
-   */
-  @RequiresPermission(Manifest.permission.INTERNET)
-  public static Observable<Boolean> observeInternetConnectivity(final int intervalInMs,
-      final String host, final int port, final int timeoutInMs) {
-    return observeInternetConnectivity(DEFAULT_INITIAL_PING_INTERVAL_IN_MS, intervalInMs, host,
-        port, timeoutInMs, new DefaultErrorHandler());
-  }
-
-  /**
-   * Observes connectivity with the Internet in a given time interval.
-   *
-   * @param initialIntervalInMs in milliseconds determining the delay of the first connectivity
-   * check
-   * @param intervalInMs in milliseconds determining how often we want to check connectivity
-   * @param host for checking Internet connectivity
-   * @param port for checking Internet connectivity
-   * @param timeoutInMs for pinging remote host in milliseconds
-   * @return RxJava Observable with Boolean - true, when we have connection with host and false if
-   * not
-   */
-  @RequiresPermission(Manifest.permission.INTERNET)
-  public static Observable<Boolean> observeInternetConnectivity(final int initialIntervalInMs,
-      final int intervalInMs, final String host, final int port, final int timeoutInMs) {
-    return observeInternetConnectivity(initialIntervalInMs, intervalInMs, host, port, timeoutInMs,
-        new DefaultErrorHandler());
-  }
-
-  /**
-   * Observes connectivity with the Internet in a given time interval.
-   *
-   * @param initialIntervalInMs in milliseconds determining the delay of the first connectivity
-   * check
-   * @param intervalInMs in milliseconds determining how often we want to check connectivity
-   * @param host for checking Internet connectivity
-   * @param port for checking Internet connectivity
-   * @param timeoutInMs for pinging remote host in milliseconds
-   * @param errorHandler for handling errors during connectivity check
-   * @return RxJava Observable with Boolean - true, when we have connection with host and false if
-   * not
-   */
-  @RequiresPermission(Manifest.permission.INTERNET)
-  public static Observable<Boolean> observeInternetConnectivity(final int initialIntervalInMs,
-      final int intervalInMs, final String host, final int port, final int timeoutInMs,
-      final ErrorHandler errorHandler) {
-    return observeInternetConnectivity(new WalledGardenInternetObservingStrategy(),
-        initialIntervalInMs, intervalInMs, host, port, timeoutInMs, errorHandler);
+      InternetObservingSettings settings) {
+    return observeInternetConnectivity(settings.strategy(), settings.initialInterval(),
+        settings.interval(), settings.host(), settings.port(),
+        settings.timeout(), settings.errorHandler());
   }
 
   /**
@@ -228,7 +139,7 @@ public class ReactiveNetwork {
    * not
    */
   @RequiresPermission(Manifest.permission.INTERNET)
-  public static Observable<Boolean> observeInternetConnectivity(
+  protected static Observable<Boolean> observeInternetConnectivity(
       final InternetObservingStrategy strategy, final int initialIntervalInMs,
       final int intervalInMs, final String host, final int port, final int timeoutInMs,
       final ErrorHandler errorHandler) {
@@ -245,70 +156,22 @@ public class ReactiveNetwork {
    */
   @RequiresPermission(Manifest.permission.INTERNET)
   public static Single<Boolean> checkInternetConnectivity() {
-    return checkInternetConnectivity(DEFAULT_PING_HOST, DEFAULT_PING_PORT,
-        DEFAULT_PING_TIMEOUT_IN_MS, new DefaultErrorHandler());
+    InternetObservingSettings settings = InternetObservingSettings.create();
+    return checkInternetConnectivity(settings.strategy(), settings.host(), settings.port(),
+        settings.timeout(), settings.errorHandler());
   }
 
   /**
    * Checks connectivity with the Internet. This operation is performed only once.
    *
-   * @param strategy which implements InternetObservingStrategy
-   * @return RxJava Single with Boolean - true, when we have an access to the Internet
-   * and false if not
-   */
-  @RequiresPermission(Manifest.permission.INTERNET)
-  public static Single<Boolean> checkInternetConnectivity(
-      final InternetObservingStrategy strategy) {
-    checkStrategyIsNotNull(strategy);
-    return strategy.checkInternetConnectivity(strategy.getDefaultPingHost(), DEFAULT_PING_PORT,
-        DEFAULT_PING_TIMEOUT_IN_MS, new DefaultErrorHandler());
-  }
-
-  /**
-   * Checks connectivity with the Internet. This operation is performed only once.
-   *
-   * @param strategy which implements InternetObservingStrategy
-   * @param host for checking Internet connectivity
-   * @return RxJava Single with Boolean - true, when we have an access to the Internet
-   * and false if not
-   */
-  public static Single<Boolean> checkInternetConnectivity(final InternetObservingStrategy strategy,
-      final String host) {
-    checkStrategyIsNotNull(strategy);
-    return strategy.checkInternetConnectivity(host, DEFAULT_PING_PORT,
-        DEFAULT_PING_TIMEOUT_IN_MS, new DefaultErrorHandler());
-  }
-
-  /**
-   * Checks connectivity with the Internet. This operation is performed only once.
-   *
-   * @param host for checking Internet connectivity
-   * @param port for checking Internet connectivity
-   * @param timeoutInMs for pinging remote host in milliseconds
+   * @param settings Internet Observing Settings created via Builder pattern
    * @return RxJava Single with Boolean - true, when we have connection with host and false if
    * not
    */
   @RequiresPermission(Manifest.permission.INTERNET)
-  public static Single<Boolean> checkInternetConnectivity(final String host, final int port,
-      final int timeoutInMs) {
-    return checkInternetConnectivity(host, port, timeoutInMs, new DefaultErrorHandler());
-  }
-
-  /**
-   * Checks connectivity with the Internet. This operation is performed only once.
-   *
-   * @param host for checking Internet connectivity
-   * @param port for checking Internet connectivity
-   * @param timeoutInMs for pinging remote host in milliseconds
-   * @param errorHandler for handling errors during connectivity check
-   * @return RxJava Single with Boolean - true, when we have connection with host and false if
-   * not
-   */
-  @RequiresPermission(Manifest.permission.INTERNET)
-  public static Single<Boolean> checkInternetConnectivity(final String host, final int port,
-      final int timeoutInMs, final ErrorHandler errorHandler) {
-    return checkInternetConnectivity(new WalledGardenInternetObservingStrategy(), host, port,
-        timeoutInMs, errorHandler);
+  public static Single<Boolean> checkInternetConnectivity(InternetObservingSettings settings) {
+    return checkInternetConnectivity(settings.strategy(), settings.host(), settings.port(),
+        settings.timeout(), settings.errorHandler());
   }
 
   /**
@@ -323,7 +186,8 @@ public class ReactiveNetwork {
    * not
    */
   @RequiresPermission(Manifest.permission.INTERNET)
-  public static Single<Boolean> checkInternetConnectivity(final InternetObservingStrategy strategy,
+  protected static Single<Boolean> checkInternetConnectivity(
+      final InternetObservingStrategy strategy,
       final String host, final int port, final int timeoutInMs, final ErrorHandler errorHandler) {
     checkStrategyIsNotNull(strategy);
     return strategy.checkInternetConnectivity(host, port, timeoutInMs, errorHandler);
---
+++ library/src/main/java/com/github/pwittchen/reactivenetwork/library/rx2/internet/observing/InternetObservingSettings.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright (C) 2018 Piotr Wittchen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.pwittchen.reactivenetwork.library.rx2.internet.observing;
+
+import com.github.pwittchen.reactivenetwork.library.rx2.internet.observing.error.DefaultErrorHandler;
+import com.github.pwittchen.reactivenetwork.library.rx2.internet.observing.error.ErrorHandler;
+import com.github.pwittchen.reactivenetwork.library.rx2.internet.observing.strategy.WalledGardenInternetObservingStrategy;
+
+/**
+ * Contains state of internet connectivity settings.
+ * We should use its Builder for creating new settings
+ */
+@SuppressWarnings("PMD") // I want to have the same method names as variable names on purpose
+public final class InternetObservingSettings {
+  private final int initialInterval;
+  private final int interval;
+  private final String host;
+  private final int port;
+  private final int timeout;
+  private final ErrorHandler errorHandler;
+  private final InternetObservingStrategy strategy;
+
+  private InternetObservingSettings(int initialInterval, int interval, String host, int port,
+      int timeout,
+      ErrorHandler errorHandler, InternetObservingStrategy strategy) {
+    this.initialInterval = initialInterval;
+    this.interval = interval;
+    this.host = host;
+    this.port = port;
+    this.timeout = timeout;
+    this.errorHandler = errorHandler;
+    this.strategy = strategy;
+  }
+
+  /**
+   * @return settings with default parameters
+   */
+  public static InternetObservingSettings create() {
+    return new InternetObservingSettings.Builder().build();
+  }
+
+  private InternetObservingSettings(Builder builder) {
+    this(builder.initialInterval, builder.interval, builder.host, builder.port, builder.timeout,
+        builder.errorHandler, builder.strategy);
+  }
+
+  private InternetObservingSettings() {
+    this(builder());
+  }
+
+  private static Builder builder() {
+    return new Builder();
+  }
+
+  /**
+   * @return initial ping interval in milliseconds
+   */
+  public int initialInterval() {
+    return initialInterval;
+  }
+
+  /**
+   * sets initial ping interval in milliseconds
+   *
+   * @param initialInterval in milliseconds
+   * @return Builder
+   */
+  public static Builder initialInterval(final int initialInterval) {
+    return builder().initialInterval(initialInterval);
+  }
+
+  /**
+   * @return ping interval in milliseconds
+   */
+  public int interval() {
+    return interval;
+  }
+
+  /**
+   * sets ping interval in milliseconds
+   *
+   * @param interval in milliseconds
+   * @return Builder
+   */
+  public static Builder interval(final int interval) {
+    return builder().interval(interval);
+  }
+
+  /**
+   * @return ping host
+   */
+  public String host() {
+    return host;
+  }
+
+  /**
+   * @return ping host
+   */
+  public static Builder host(final String host) {
+    return builder().host(host);
+  }
+
+  /**
+   * @return ping port
+   */
+  public int port() {
+    return port;
+  }
+
+  /**
+   * sets ping port
+   *
+   * @return Builder
+   */
+  public static Builder port(final int port) {
+    return builder().port(port);
+  }
+
+  /**
+   * @return ping timeout in milliseconds
+   */
+  public int timeout() {
+    return timeout;
+  }
+
+  /**
+   * sets ping timeout in milliseconds
+   *
+   * @param timeout in milliseconds
+   * @return Builder
+   */
+  public static Builder timeout(final int timeout) {
+    return builder().timeout(timeout);
+  }
+
+  /**
+   * @return error handler for pings and connections
+   */
+  public ErrorHandler errorHandler() {
+    return errorHandler;
+  }
+
+  /**
+   * sets error handler for pings and connections
+   *
+   * @return Builder
+   */
+  public static Builder errorHandler(final ErrorHandler errorHandler) {
+    return builder().errorHandler(errorHandler);
+  }
+
+  /**
+   * @return internet observing strategy
+   */
+  public InternetObservingStrategy strategy() {
+    return strategy;
+  }
+
+  /**
+   * sets internet observing strategy
+   *
+   * @param strategy for observing and internet connection
+   * @return Builder
+   */
+  public static Builder strategy(final InternetObservingStrategy strategy) {
+    return builder().strategy(strategy);
+  }
+
+  /**
+   * Settings builder, which contains default parameters
+   */
+  public final static class Builder {
+    private int initialInterval = 0;
+    private int interval = 2000;
+    private String host = "http://clients3.google.com/generate_204";
+    private int port = 80;
+    private int timeout = 2000;
+    private ErrorHandler errorHandler = new DefaultErrorHandler();
+    private InternetObservingStrategy strategy = new WalledGardenInternetObservingStrategy();
+
+    private Builder() {
+    }
+
+    public Builder initialInterval(int initialInterval) {
+      this.initialInterval = initialInterval;
+      return this;
+    }
+
+    public Builder interval(int interval) {
+      this.interval = interval;
+      return this;
+    }
+
+    public Builder host(String host) {
+      this.host = host;
+      return this;
+    }
+
+    public Builder port(int port) {
+      this.port = port;
+      return this;
+    }
+
+    public Builder timeout(int timeout) {
+      this.timeout = timeout;
+      return this;
+    }
+
+    public Builder errorHandler(ErrorHandler errorHandler) {
+      this.errorHandler = errorHandler;
+      return this;
+    }
+
+    public Builder strategy(InternetObservingStrategy strategy) {
+      this.strategy = strategy;
+      return this;
+    }
+
+    public InternetObservingSettings build() {
+      return new InternetObservingSettings(this);
+    }
+  }
+}
