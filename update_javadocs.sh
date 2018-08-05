7ce764a61646cd35a487e78319d2c9390256f9a8
Piotr Wittchen <piotr@wittchen.io>
2018-08-05 16:49:53 +0200

    making messages in `update_javadocs.sh` script consistent

--- update_javadocs.sh
+++ update_javadocs.sh
@@ -18,7 +18,7 @@ git checkout gh-pages
 rm -rf javadoc/RxJava1.x/*
 cp -avr library/build/docs/javadoc/* ./javadoc/RxJava1.x
 git add -A
-git commit -m "updating javadocs for RxJava1.x"
+git commit -m "updating JavaDoc for RxJava1.x"
 echo "javadocs for RxJava1.x updated"
 
 echo "javadocs for both RxJava1.x and RxJava2.x updated - now you can push your changes"
