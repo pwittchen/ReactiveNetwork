c370d95ee1e0c4b297279459cb50ac92e0d3d120
Piotr Wittchen <piotr@wittchen.io>
2019-08-07 10:00:48 +0200

    removing RxJava1.x stuff from update_docs.sh script

--- update_docs.sh
+++ update_docs.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 git checkout gh-pages
-git show RxJava1.x:README.md >docs/RxJava1.x/README.md
 git show RxJava2.x:README.md >docs/RxJava2.x/README.md
 git add -A
 git commit -m "updating docs"
